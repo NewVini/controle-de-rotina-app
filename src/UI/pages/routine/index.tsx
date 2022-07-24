304a01a673e90811d9d28235b3c3ec135a82d1e6
vinieu <vincius.machado8@gmail.com>
2022-07-24 17:35:03 -0300

    feature: finalize mocked days functionalitys

--- src/UI/pages/routine/index.tsx
+++ src/UI/pages/routine/index.tsx
@@ -2,8 +2,20 @@
 import { useEffect, useState } from 'react'
 import './index.scss'
 import { daysWeekMock } from '../../../utils/daysWeekMock'
+import api from '../../../services/api'
+
 export const RoutinePage = () => {
     const [arrayDaysWeek] = useState(daysWeekMock)
+
+    const getDaysAndTasks = () => {
+        api.get("activity").then(res => {
+            console.log(res)
+        })
+    }
+
+    useEffect(() => {
+        getDaysAndTasks();
+    }, [])
 
     return (
         <>
@@ -17,15 +29,15 @@ export const RoutinePage = () => {
                         <h1 className='row-h1'> Missões diárias</h1>
                     </div>
                 </div>
-                <div className="row mt-4">
+                <div className="row mt-4 pt-5">
                     <form >
                         <input placeholder='Insira uma missão' type="text" />
                         <select name="" id="">
                             {
                                 <>
                                     <option value="" selected disabled hidden >Dia da semana</option>
-                                    {arrayDaysWeek.map( (daysObj,i) => {
-                                      return <option key={i} value={daysObj.id}> {daysObj.name} </option>
+                                    {arrayDaysWeek.map((daysObj, i) => {
+                                        return <option key={i} value={daysObj.id}> {daysObj.name} </option>
                                     })}
                                 </>
                             }
@@ -33,17 +45,22 @@ export const RoutinePage = () => {
                         <button type='submit'>Cadastrar</button>
                     </form>
                 </div>
-                <div className="row mt-4">
-                    <div className="col-md-12 d-flex justify-content-between">
-                            <div className='day-container'>
-                                <div className="day-title">
-                                    <h1>Segunda</h1>
+                <div className="row mt-5">
+                    <div className="col-md-12 d-flex justify-content-between pt-5">
+                        {
+                            arrayDaysWeek.map((daysObj, i) => {
+                                return <div key={i} className='day-container'>
+                                    <div className="day-title">
+                                        <h1> {daysObj.name} </h1>
+                                    </div>
+                                    {
+                                        <div className="day-tasks">
+                                            <input type="checkbox" name="" id="" />
+                                            <label>Estudar</label>
+                                        </div>
+                                    }
                                 </div>
-                                <div className="day-tasks">
-                                    <input type="checkbox" name="" id=""  />
-                                    <label>Estudar</label>
-                                </div>
-                            </div>
+                            })}
                     </div>
                 </div>
             </div>
